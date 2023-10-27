7f1371edebbb89ccc81449cfc6c943dec589f6e6
tehpsalmist <bstewardcodes@gmail.com>
2023-10-26 23:37:42 -0400

    ok let's try again

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV !== 'development') {
 }
 
 let root
-export function init({
+export async function init({
   container,
   props,
 }: {
@@ -23,6 +23,18 @@ export function init({
 }) {
   if (container) {
     root = createRoot(container)
+
+    if (typeof (window as any).dmAPI === 'undefined') {
+      return console.error(
+        'no dmAPI object available to bootstrap widget content'
+      )
+    }
+
+    const dp = (window as any).dmAPI.dynamicPageApi()
+
+    if (!dp.isDynamicPage()) return console.error('not on a dynamic page')
+
+    props.pageData = await dp.pageData()
 
     root.render(
       <DudaProvider value={props}>
@@ -55,6 +67,35 @@ if (process.env.NODE_ENV === 'development') {
           elementId: '1789939675',
           config: {},
           locale: 'en',
+        },
+        pageData: {
+          business_name: 'Aloha Adventures',
+          images: [
+            {
+              url: 'https://irt-cdn.multiscreensite.com/3105c6ea9c524f6482a10797a4aa680a/dms3rep/multi/Cat_August_2010-4-b3e1cbd3.jpg',
+              status: 'UPLOADED',
+              original_url:
+                'https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg',
+            },
+          ],
+          island: 'oahu',
+          primary_address: '1234 Beach Blvd, Honolulu, HI',
+          created_at: '',
+          description:
+            "Experience the thrill of surfing on the beautiful shores of Oahu. Our expert instructors will have you riding the waves in no time. Book your adventure today and discover the magic of the Pacific Ocean. Whether you're a beginner or an experienced surfer, our friendly team is here to make your surfing dreams come true. Join us for a memorable day on the water and ride the waves of Aloha!",
+          videos: [
+            {
+              url: 'https://www.youtube.com/watch?v=ik2Z1IZPJZ8',
+            },
+          ],
+          booking_links: [],
+          primary_phone: '808-555-1234',
+          updated_at: '',
+          tier: 'basic',
+          primary_email: 'info@alohaadventures.com',
+          id: 1,
+          slogan: 'Catch the Wave of Fun!',
+          primary_web_url: 'https://www.alohaadventures.com',
         },
       }}
     >
