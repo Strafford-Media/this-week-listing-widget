de476715de297f2213b1187207a7ef1ff383a778
tehpsalmist <bstewardcodes@gmail.com>
2024-02-29 23:11:12 -0500

    remove empty space

--- src/components/BookingLinks.tsx
+++ src/components/BookingLinks.tsx
@@ -37,8 +37,8 @@ export const BookingLinks = ({ className = '', links, ...props }: BookingLinksPr
           .filter((link) => link.type !== 'fareharbor-grid')
           .map((link) => (
             <div className="tw-flex tw-w-full tw-max-w-80 tw-flex-col tw-items-center tw-gap-4 tw-rounded-md tw-border tw-border-gray-300 tw-p-2 tw-shadow-md">
-              <h4>{link.title}</h4>
-              <div className="tw-text-justify tw-text-sm">{link.description}</div>
+              {link.title && <h4>{link.title}</h4>}
+              {link.description && <div className="tw-text-justify tw-text-sm">{link.description}</div>}
               {link.type === 'fareharbor-item' && <FareHarborItemLink className="tw-mt-auto" link={link} />}
               {link.type === 'external' && <ExternalBookingLink className="tw-mt-auto" link={link} />}
             </div>
