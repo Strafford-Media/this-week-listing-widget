5e3ab6bc00551046d5264c52ad6ee07c0a73cbf1
tehpsalmist <bstewardcodes@gmail.com>
2024-06-27 18:48:16 -0400

    lowercase before removing

--- src/components/ListingList.tsx
+++ src/components/ListingList.tsx
@@ -117,7 +117,11 @@ export const ListingList = ({ className = '', ...props }: ListingListProps) => {
               className={`tw-relative tw-bottom-0 tw-rounded-full tw-px-2 tw-py-0.5 tw-text-sm tw-capitalize before:tw-absolute before:tw-inset-0 before:tw-z-10 before:tw-rounded-full before:tw-bg-gray-700 before:tw-opacity-0 after:tw-absolute after:tw-inset-0 after:tw-z-20 after:tw-py-0.5 after:tw-font-bold after:tw-opacity-0 after:tw-content-x hover:before:tw-opacity-50 hover:after:tw-opacity-100 focus:tw-outline-none focus:tw-ring-2 focus:before:tw-opacity-50 focus:after:tw-opacity-100 ${islandClasses[island]?.coloredBg}`}
               onClick={() => {
                 if (cat === category) {
-                  navigate({ path: window.location.pathname.replace(`/${encodeURIComponent(category)}`, '') })
+                  navigate({
+                    path: window.location.pathname
+                      .toLowerCase()
+                      .replace(`/${encodeURIComponent(category.toLowerCase())}`, ''),
+                  })
                 } else {
                   navigate({ remove: { category: [cat] } })
                 }
